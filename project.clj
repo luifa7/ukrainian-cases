a759ba12a4ea27f0486f0159b2862c8a89a8dc08
luifa <luifa.luifa@gmail.com>
2020-11-22 13:07:38 +0100

    add initial file

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject ukrainian_cases "0.1.0-SNAPSHOT"
+  :description "Project to get all the cases forms for a given ukrainian word"
+  :url "https://github.com/luifa7/ukrainian-cases"
+  :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
+            :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :main ^:skip-aot ukrainian-cases.core
+  :target-path "target/%s"
+  :profiles {:uberjar {:aot :all
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
