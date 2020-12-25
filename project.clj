fd46386149a0c7d9697427b0199517d826082fe3
luifa <luifa.luifa@gmail.com>
2020-12-25 13:24:45 +0100

    add initial api components

--- project.clj
+++ project.clj
@@ -1,10 +1,18 @@
-(defproject ukrainian_cases "0.1.0-SNAPSHOT"
+(defproject ukrainian_cases "0.2.0-SNAPSHOT"
   :description "Project to get all the cases forms for a given ukrainian word"
   :url "https://github.com/luifa7/ukrainian-cases"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :dependencies [[org.clojure/clojure "1.10.1"]
+                 [compojure "1.6.1"]
+                 [ring/ring-defaults "0.3.2"]
+                 [org.clojure/data.json "1.0.0"]]
+  :plugins [[lein-ring "0.12.5"]]
+  :ring {:handler ukrainian-cases.handler/app}
   :main ^:skip-aot ukrainian-cases.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
-                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}})
+                       :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
+             :dev {:dependencies [[javax.servlet/servlet-api "2.5"]
+                                  [ring/ring-mock "0.3.2"]]}}
+  )
--- src/ukrainian_cases/handler.clj
+++ src/ukrainian_cases/handler.clj
@@ -1,11 +1,25 @@
-(ns clojure-rest.handler
+(ns ukrainian-cases.handler
   (:require [compojure.core :refer :all]
             [compojure.route :as route]
-            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]))
+            [ring.middleware.defaults :refer [wrap-defaults site-defaults]]
+            [ukrainian-cases.cases.nominative :refer [get-possible-genders-nominative
+                                                      get-possible-plurals-nominative]]
+            [ukrainian-cases.util :refer [to-json]]))
 
 (defroutes app-routes
-  (GET "/" [] "Hello World")
-  (route/not-found "Not Found"))
+  (context "/nouns" []
+    (defroutes nouns-routes
+     (GET  "/" [] "Hello Nouns")
+     (context "/:noun" [noun]
+      (defroutes noun-routes
+       (GET  "/" [] noun)
+       (GET  "/gender" [] (to-json (get-possible-genders-nominative noun)))
+       (GET  "/nominative" [] (to-json (get-possible-plurals-nominative noun)))
+       ))))
+
+ (GET "/" [] "Hello World")
+ (route/not-found "Not Found")
+ )
 
 (def app
   (wrap-defaults app-routes site-defaults))
