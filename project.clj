7c28562f260bd990da70a40c72eca256bdf59dfc
luifa <luifa.luifa@gmail.com>
2020-12-25 14:35:54 +0100

    add main to run server

--- project.clj
+++ project.clj
@@ -6,10 +6,13 @@
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [compojure "1.6.1"]
                  [ring/ring-defaults "0.3.2"]
+                 [ring/ring-core "1.7.1"]
+                 [ring/ring-jetty-adapter "1.7.1"]
                  [org.clojure/data.json "1.0.0"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler ukrainian-cases.handler/app}
-  :main ^:skip-aot ukrainian-cases.core
+  :main ukrainian-cases.core
+  :aot [ukrainian-cases.core]
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all
                        :jvm-opts ["-Dclojure.compiler.direct-linking=true"]}
--- src/ukrainian_cases/core.clj
+++ src/ukrainian_cases/core.clj
@@ -1,20 +1,8 @@
 (ns ukrainian-cases.core
-  (:gen-class
-    :name ukrainian.cases.core
-    :methods
-    [#^{:static true} [getGendersNominative [String] java.util.HashMap]
-     #^{:static true} [getPluralsNominative [String] java.util.HashMap]])
-  (:require [ukrainian-cases.cases.nominative :refer [get-possible-genders-nominative
-                                                      get-possible-plurals-nominative]]
-            [ukrainian-cases.util :refer [convert-to-java-hashmap]])
+  (:gen-class)
+  (:use ring.adapter.jetty)
+  (:require [ukrainian-cases.handler :refer [app] ])
   )
 
-(defn -getGendersNominative
-  "A Java-callable wrapper around the 'get-possible-genders-nominative' function."
-  [word]
-  (convert-to-java-hashmap (get-possible-genders-nominative word)))
-
-(defn -getPluralsNominative
-  "A Java-callable wrapper around the 'get-get-possible-plurals-nominative-genders-nominative' function."
-  [word]
-  (convert-to-java-hashmap (get-possible-plurals-nominative word)))
+(defn -main [& args]
+  (run-jetty app {:port 3000}))
