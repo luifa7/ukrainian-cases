e83d846cb5fc95cefecb4cd6e0ca9b9866782a28
luifa <luifa.luifa@gmail.com>
2020-12-26 12:42:57 +0100

    change port input to work with heroku

--- src/ukrainian_cases/core.clj
+++ src/ukrainian_cases/core.clj
@@ -1,8 +1,10 @@
 (ns ukrainian-cases.core
   (:gen-class)
-  (:use ring.adapter.jetty)
-  (:require [ukrainian-cases.handler :refer [app] ])
+  (:require [ukrainian-cases.handler :refer [app]]
+            [environ.core :refer [env]]
+            [ring.adapter.jetty :as jetty])
   )
 
-(defn -main [& args]
-  (run-jetty app {:port 3000}))
+(defn -main [& [port]]
+  (let [port (Integer. (or port (env :port) 5000))]
+    (jetty/run-jetty app {:port port :join? false})))
